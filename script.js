ab23827d29f7b3ef00fcd4683df9b2aad0b2fe33
Aciel Olivera <acielito64@gmail.com>
2025-11-14 08:04:48 +0100

    archivo .htaccess añadido para seguridad en las fotos

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 /* Copiar al portapapeles - No soportado en todos los navegadores */
 function clipboardCopy() {
   let texto = document.getElementById("copiar-codigo").innerText;
-    navigator.clipboard.writeText(texto).then(function() {
-        let feedbackElement = document.getElementById("feedbackElement")
-        feedbackElement.innerHTML += `<i class="fa-regular fa-clipboard"></i><p>Texto copiado al portapapeles</p>`;
-    }).catch(function(err) {
-        feedbackElement.textContent = 'Error al copiar texto: ' + err;
+  let feedbackElement = document.getElementById("feedbackElement");
+
+  navigator.clipboard.writeText(texto)
+    .then(function () {
+
+      // Mostrar mensaje
+      feedbackElement.innerHTML = `
+        <i class="fa-regular fa-clipboard"></i>
+        <p>Texto copiado al portapapeles</p>
+      `;
+
+      // Ocultar después de 1 segundo
+      setTimeout(() => {
+        feedbackElement.innerHTML = "";
+      }, 2000);
+
+    })
+    .catch(function (err) {
+      feedbackElement.textContent = 'Error al copiar texto: ' + err;
+
+      setTimeout(() => {
+        feedbackElement.innerHTML = "";
+      }, 1000);
     });
 }
 
- document.addEventListener("DOMContentLoaded", function () {
-      document.getElementById("copiar-codigo").addEventListener("click", clipboardCopy);
-    });
-
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("copiar-codigo").addEventListener("click", clipboardCopy);
+});
